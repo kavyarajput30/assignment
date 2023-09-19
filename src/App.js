f5db50b344894dc6e2389f57d32886662faeeeee
kavya rajput <131941640+rajputrajput30@users.noreply.github.com>
2023-09-19 23:32:35 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Navbar from './components/navbar/Navbar';
+import Topview from './components/topview/TopView'
+
+function App() {
+  return (
+     <div>
+      <Navbar/>
+      <Topview/>
+     </div>
+  );
+}
+
+export default App;
